df4beebcf5c444ee227d4acd86b8fa39943e793a
康鑫 <k506855660@163.com>
2017-10-02 23:10:22 +0800

    add singe file setup.py Usage

---
+++ setup_test/setup_single.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='grape',
+    description='just a grape',
+    author='kagxin',
+    author_email='kagxin@gmail.com',
+    py_modules=['grape'],
+    )
