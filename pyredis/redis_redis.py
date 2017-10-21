ed0eb4fae737d167cfd05d24a96c7330bd3eedf4
康鑫 <k506855660@163.com>
2017-10-21 12:08:01 +0800

    pubsub run in thread

--- pyredis/redis_redis.py
+++ pyredis/redis_redis.py
@@ -1,5 +1,16 @@
 import redis
 
 
-r = redis.Redis(host='localhost',port=6379,db=0)
-print(r.keys())
+def handler(message):
+    print(message)
+
+
+if __name__ == '__main__':
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    p = r.pubsub()
+    p.subscribe(**{'channel1': handler})
+
+    thread = p.run_in_thread(sleep_time=0.001)
+
+    r.publish('channel1', 'this will reach the listener')
+
