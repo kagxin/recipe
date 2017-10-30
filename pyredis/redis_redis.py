28ccc529cd61e972ef489f50516c636eb2ee70d1
康鑫 <k506855660@163.com>
2017-10-30 23:20:54 +0800

    modify redis_redis

--- pyredis/redis_redis.py
+++ pyredis/redis_redis.py
@@ -4,11 +4,16 @@ import redis
 def handler(message):
     print(message)
 
+def handler2(message):
+    print('msg2:{}'.format(message))
 
 if __name__ == '__main__':
+
     r = redis.Redis(host='localhost', port=6379, db=0)
     p = r.pubsub()
     p.subscribe(**{'channel1': handler})
+    # p.subscribe(**{'channel1': handler2})
+
 
     thread = p.run_in_thread(sleep_time=0.001)
 
