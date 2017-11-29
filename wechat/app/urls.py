eaf0f361428825ee6ee8d6be4c005c8dc465350e
kagxin <k506855660@163.com>
2017-11-29 11:19:54 +0800

    add wechat

---
+++ wechat/app/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, include
+from app import views
+
+urlpatterns = [
+    url(r'^test/', views.TestView.as_view())
+]
--- wechat/app/views.py
+++ wechat/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class TestView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('test')
