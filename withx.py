2eae97e9c528f9b7ea08d0b20cc8f7032a61ca7d
kagxin <k506855660@163.com>
2018-02-28 15:34:26 +0800

    '上下文管理器的一个妙用'

--- withx.py
+++ withx.py
@@ -16,10 +16,49 @@ class WriteFile:
         return
 
 
-with WriteFile():
-    print('test')
+# with WriteFile():
+#     print('test')
     # f = open('../test', 'w+')
     # f.write('saf')
     # f.close()
-
 # print('exdit')
+
+
+import threading
+
+class JustRunTimeoutCM(threading.Thread):
+    """timeout之后，给上下文管理器中的，区块发送keyboard interrupt信号, 
+    """
+    def __init__(self, timeout=5):
+        threading.Thread.__init__(self)
+        self.timeout = timeout
+
+    def run(self):
+        import time
+        try:
+            import thread as thread
+        except:
+            import _thread as thread
+        current_time = time.time()
+        while time.time() < current_time + self.timeout:
+            time.sleep(0.1)
+        thread.interrupt_main()
+
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, exc_type, *_):
+        self.join()
+        return exc_type is not None and issubclass(exc_type, KeyboardInterrupt)
+
+if __name__ == '__main__':
+    print('print begin.')
+
+    with JustRunTimeoutCM(timeout=5):
+        while True:
+            import time
+            time.sleep(1)
+            print('alive...')
+
+    print('print end.')    
+
